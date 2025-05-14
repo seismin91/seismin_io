85c5e600e36af356889dd037685ac854ebdc5640
Sumin Kim <143072733+seismin91@users.noreply.github.com>
2025-05-14 12:32:53 +0900

    Distribution

--- README.md
+++
@@ -1,2 +0,0 @@
-# seismin_io
-Python I/O module for seismic dataset with Seismic Un*x format
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='suio',
+    version='0.1.0',
+    description='Python I/O module for seismic dataset with Seismic Un*x format',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Dr. Sumin Kim',
+    author_email='seisminic@gmail.com',
+    url='https://github.com/seismin91',
+    py_modules=['suio'],
+    include_package_data=True,  # 중요!
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU License',
+        'Intended Audience :: Science/Research',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+    ],
+)
